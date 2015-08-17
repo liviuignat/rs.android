2f1229dd694d95cf8f9e5f120a30df80f452cfb8
Liviu Ignat <liviubiker@yahoo.com>
2015-08-17 19:55:01 +0200

    Still trying to mock static method

--- app/src/test/java/com/c24/rs/app/screens/search/When_SearchActivity_SearchButton_Is_Clicked.java
+++ app/src/test/java/com/c24/rs/app/screens/search/When_SearchActivity_SearchButton_Is_Clicked.java
@@ -1,24 +1,43 @@
 package com.c24.rs.app.screens.search;
 
 import com.c24.rs.BuildConfig;
+import com.c24.rs.app.screens.tariffList.TariffListActivity;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
+@PrepareForTest(TariffListActivity.class)
 public class When_SearchActivity_SearchButton_Is_Clicked {
-    SearchActivity activity;
+    SearchActivity_ activity;
 
     @Before
     public void setUp() {
-        //activity = Robolectric.setupActivity(SearchActivity.class);
+        activity = Robolectric.setupActivity(SearchActivity_.class);
     }
 
     @Test
     public void should_show_tariff_list() throws Exception {
+        PowerMockito.mockStatic(TariffListActivity.class);
+        
+        PowerMockito.verifyStatic(Mockito.times(100));
+        activity.getSearchTariffQuery();
+
+
+//        SearchActivity_ spy = Mockito.spy(SearchActivity_.class);
+//
+//        Mockito.doReturn(new SearchTariffQuery()).when(spy).getSearchTariffQuery();
+//
+//        spy.searchButtonClicked();
+//
+//        Mockito.verify(spy).getSearchTariffQuery();
     }
 }
